27eb080d3d8955b28798c3f3ec33593100cfe092
fffelix-huang <felixhuang07@gmail.com>
2023-04-17 03:49:54 +0800

    QQ

--- codes/Misc/default.cpp
+++
@@ -1,9 +0,0 @@
-#define all(v) (v).begin(), (v).end()
-#define sz(v) ((int)v.size())
-#define pb push_back
-#define eb emplace_back
-#define ppb pop_back
-#define REP(i, n) for(int i = 0; i < int(n); i++)
-
-using vi = vector<int>;
-using pii = pair<int, int>;
