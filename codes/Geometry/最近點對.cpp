796dcfafccb8fe33a00d7a244fda426346299439
WeakMouse <felixhuang07@gmail.com>
2023-04-27 22:21:35 +0800

    QQ

--- codes/Geometry/最近點對.cpp
+++ codes/Geometry/最近點對.cpp
@@ -1,20 +1,21 @@
-const ll INF = 9e18L;
+const ll INF = 9e18L + 5;
 vector<P> a;
 sort(all(a), [](P a, P b) { return a.x < b.x; });
+ll SQ(ll x) { return x * x; }
 ll solve(int l, int r) {
 	if(l + 1 == r) return INF;
 	int m = (l + r) / 2;
+	ll midx = a[m].x;
 	ll d = min(solve(l, m), solve(m, r));
 	inplace_merge(a.begin() + l, a.begin() + m, a.begin() + r, [](P a, P b) {
 		return a.y < b.y;
 	});
-	#define SQ(x) ((x) * (x))
 	vector<P> p;
-	for(int i = l; i < r; ++i) if(SQ(a[i].x - a[m].x) <= d) p.pb(a[i]);
+	for(int i = l; i < r; ++i) if(SQ(a[i].x - midx) < d) p.pb(a[i]);
 	REP(i, sz(p)) {
 		for(int j = i + 1; j < sz(p); ++j) {
-			if((p[i].y - p[j].y) * (p[i].y - p[j].y) > d) break;
-			d = min(d, SQ(p[i].x - p[j].x) + SQ(p[i].y - p[j].y));
+		    d = min(d, SQ(p[i].x - p[j].x) + SQ(p[i].y - p[j].y));
+			if(SQ(p[i].y - p[j].y) > d) break;
 		}
 	}
 	return d; // 距離平方
