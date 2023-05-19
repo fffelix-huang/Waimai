4edd929ababa79142e8e05da0f3b269b7cc0681a
fffelix-huang <felixhuang07@gmail.com>
2023-05-19 19:35:38 +0800

    Waimai

--- codes/String/rolling-hash.cpp
+++ codes/String/rolling-hash.cpp
@@ -1,5 +1,5 @@
 const ll M = 911382323, mod = 972663749;
-ll Get(vector<ll>& v, int l, int r) {
+ll Get(vector<ll>& h, int l, int r) {
 	if(!l) return h[r]; // p[i] = M^i % mod
 	ll ans = (h[r] - h[l - 1] * p[r - l + 1]) % mod;
 	return (ans + mod) % mod;
